c8ba1cc1e8068bdade15eaef64100104b588e237
Lucas Barbosa <barbosals298@gmail.com>
2025-06-01 21:17:19 -0300

    Delete script2.js

--- script2.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("Página carregada com sucesso!");
-
-  // Mensagem de boas-vindas (exemplo simples)
-  setTimeout(() => {
-    alert("Bem-vindo ao site da System Solutions!");
-  }, 1000);
-});
